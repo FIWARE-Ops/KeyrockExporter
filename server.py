f5e1ed34dcfdf47cbff2774122a41335ee4800fe
Dmitrii Demin <mail@demin.co>
2019-08-23 14:50:40 +0200

    small refactor

--- server.py
+++ server.py
@@ -14,6 +14,71 @@ import argparse
 from urllib.parse import unquote
 
 
+def assign_hash(value, target):
+    for el in config:
+        if config[el]['target'] == target:
+            config[value] = dict()
+            config[value] = config.pop(el)
+            return True
+
+    return False
+
+
+def check(target):
+    headers = dict()
+    entities = dict()
+
+    trg = config[target]['target']
+
+    if 'access_token' in config[target]:
+        headers['x-auth-token'] = config[target]['access_token']
+
+    url = trg + '/version'
+
+    try:
+        resp = requests.get(url, headers=headers, timeout=config[target]['timeout'])
+    except requests.exceptions.RequestException:
+        return False, "orion_failed_check_version_timeout"
+
+    if not resp.status_code == 200:
+        return False, "orion_failed_check_version_response_code"
+
+    if 'entities' in config[target]:
+        for el in config[target]['entities']:
+            url = trg + '/v2/entities?limit=1'
+            if 'fiware-service' in headers:
+                del headers['fiware-service']
+            if 'fiware-servicepath' in headers:
+                del headers['fiware-servicepath']
+
+            if 'id' in config[target]['entities'][el]:
+                url = trg + '/v2/entities/' + config[target]['entities'][el]['id']
+            elif 'type' in config[target]['entities'][el]:
+                url = trg + '/v2/entities?limit=1&type=' + config[target]['entities'][el]['type']
+
+            if 'service' in config[target]['entities'][el]:
+                headers['fiware-service'] = config[target]['entities'][el]['service']
+
+            if 'path' in config[target]['entities'][el]:
+                headers['fiware-servicepath'] = config[target]['entities'][el]['path']
+
+            try:
+                resp = requests.get(url, headers=headers, timeout=config[target]['timeout'])
+            except requests.exceptions.RequestException:
+                return False, "orion_failed_check_entity_timeout"
+
+            if resp.status_code == 200:
+                tmp = len(jsn.loads(resp.text))
+                if 'id' in config[target]['entities'][el] and tmp > 0:
+                    entities[el] = 1
+                else:
+                    entities[el] = tmp
+            else:
+                return False, "orion_failed_check_entity_response_code"
+
+    return True, entities
+
+
 def parse_request_line(request_line):
     request_line = unquote(unquote(request_line.split('HTTP')[0].strip()))
     cmd = request_line.split('/')[1].strip().split('?')[0]
@@ -32,18 +97,18 @@ def parse_request_line(request_line):
 
 
 def request_token(target, sel):
-    auth = requests.auth.HTTPBasicAuth(data[target]['client_id'], data[target]['client_secret'])
+    auth = requests.auth.HTTPBasicAuth(config[target]['client_id'], config[target]['client_secret'])
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-    url = data[target]['keyrock'] + '/oauth2/token'
+    url = config[target]['keyrock'] + '/oauth2/token'
 
     if sel == 'request':
         payload = {'grant_type': 'password',
-                   'username': data[target]['username'],
-                   'password': data[target]['password']}
+                   'username': config[target]['username'],
+                   'password': config[target]['password']}
     elif sel == 'refresh':
         payload = {'grant_type': 'refresh_token',
-                   'refresh_token': data[target]['refresh_token']}
+                   'refresh_token': config[target]['refresh_token']}
 
     try:
         resp = requests.post(url, auth=auth, data=payload, headers=headers, timeout=timeout_keyrock)
@@ -52,14 +117,14 @@ def request_token(target, sel):
 
     if resp.status_code == 200:
         reply = jsn.loads(resp.text)
-        data[target]['access_token'] = reply['access_token']
-        data[target]['refresh_token'] = reply['refresh_token']
+        config[target]['access_token'] = reply['access_token']
+        config[target]['refresh_token'] = reply['refresh_token']
         return True, None
     return False, "orion_failed_" + sel + "_token_response_code"
 
 
 def validate_token(target):
-    url = data[target]['keyrock'] + '/user?access_token=' + data[target]['access_token']
+    url = config[target]['keyrock'] + '/user?access_token=' + config[target]['access_token']
     try:
         resp = requests.get(url, timeout=timeout_keyrock)
     except requests.exceptions.RequestException:
@@ -71,103 +136,7 @@ def validate_token(target):
         return False, "orion_failed_validate_token_response_code"
 
 
-def assign_hash(value, target):
-    for el in data:
-        if data[el]['target'] == target:
-            data[value] = dict()
-            data[value] = data.pop(el)
-            return True
-
-    return False
-
-
-def check(target):
-    headers = dict()
-    entities = dict()
-
-    trg = data[target]['target']
-
-    if 'access_token' in data[target]:
-        headers['x-auth-token'] = data[target]['access_token']
-
-    url = trg + '/version'
-
-    try:
-        resp = requests.get(url, headers=headers, timeout=data[target]['timeout'])
-    except requests.exceptions.RequestException:
-        return False, "orion_failed_check_version_timeout"
-
-    if not resp.status_code == 200:
-        return False, "orion_failed_check_version_response_code"
-
-    if 'entities' in data[target]:
-        for el in data[target]['entities']:
-            url = trg + '/v2/entities?limit=1'
-            if 'fiware-service' in headers:
-                del headers['fiware-service']
-            if 'fiware-servicepath' in headers:
-                del headers['fiware-servicepath']
-
-            if 'id' in data[target]['entities'][el]:
-                url = trg + '/v2/entities/' + data[target]['entities'][el]['id']
-            elif 'type' in data[target]['entities'][el]:
-                url = trg + '/v2/entities?limit=1&type=' + data[target]['entities'][el]['type']
-
-            if 'service' in data[target]['entities'][el]:
-                headers['fiware-service'] = data[target]['entities'][el]['service']
-
-            if 'path' in data[target]['entities'][el]:
-                headers['fiware-servicepath'] = data[target]['entities'][el]['path']
-
-            try:
-                resp = requests.get(url, headers=headers, timeout=data[target]['timeout'])
-            except requests.exceptions.RequestException:
-                return False, "orion_failed_check_entity_timeout"
-
-            if resp.status_code == 200:
-                tmp = len(jsn.loads(resp.text))
-                if 'id' in data[target]['entities'][el] and tmp > 0:
-                    entities[el] = 1
-                else:
-                    entities[el] = tmp
-            else:
-                return False, "orion_failed_check_entity_response_code"
-
-    return True, entities
-
-
 class Handler(http.server.BaseHTTPRequestHandler):
-
-    def reply(self, reply=dict(), silent=False, code=200, cmd=''):
-        self.send_response(code)
-        self.send_header('content-type', 'text/plain')
-        self.end_headers()
-        if cmd in ['ping', 'version']:
-            message = jsn.dumps(reply)
-        else:
-            message = ''
-            for el in sorted(reply):
-                message = message + el + ' ' + str(reply[el]) + '\n'
-
-        self.wfile.write(bytes(message, 'utf8'))
-        log = dict()
-        if not silent:
-            log['code'] = code
-            if self.headers.get('X-Real-IP'):
-                log['ip'] = self.headers.get('X-Real-IP')
-            else:
-                log['ip'] = self.client_address[0]
-            log['request'] = unquote(unquote(self.requestline))
-            log['date'] = datetime.datetime.now().isoformat()
-            if cmd:
-                log['cmd'] = cmd
-            if reply:
-                log['message'] = reply
-            print(jsn.dumps(log, indent=2))
-        return
-
-    def log_message(self, format, *args):
-        return
 
     def do_GET(self):
         message = schema.copy()
@@ -191,15 +160,15 @@ class Handler(http.server.BaseHTTPRequestHandler):
 
         if cmd == 'probe' and 'target' in param:
             target = hash(param['target']) % ((sys.maxsize + 1) * 2)
-            if target not in data:
+            if target not in config:
                 if not assign_hash(target, param['target']):
                     message['orion_failed_wrong_target'] = 1
                     self.reply(message, cmd=cmd)
                     return
-            if 'keyrock' in data[target]:
+            if 'keyrock' in config[target]:
                 start_time = time.time()
 
-                if 'access_token' not in data[target]:
+                if 'access_token' not in config[target]:
                     status, reason = request_token(target, 'request')
                     if not status:
                         message[reason] = 1
@@ -250,6 +219,37 @@ class Handler(http.server.BaseHTTPRequestHandler):
         self.reply(message, cmd=cmd, code=404)
         return
 
+    def log_message(self, format, *args):
+        return
+
+    def reply(self, reply=None, silent=False, code=200, cmd=None):
+        self.send_response(code)
+        self.send_header('content-type', 'text/plain')
+        self.end_headers()
+        if cmd in ['ping', 'version']:
+            message = jsn.dumps(reply)
+        else:
+            message = ''
+            for el in sorted(reply):
+                message = message + el + ' ' + str(reply[el]) + '\n'
+
+        self.wfile.write(bytes(message, 'utf8'))
+        log = dict()
+        if not silent:
+            log['code'] = code
+            if self.headers.get('X-Real-IP'):
+                log['ip'] = self.headers.get('X-Real-IP')
+            else:
+                log['ip'] = self.client_address[0]
+            log['request'] = unquote(unquote(self.requestline))
+            log['date'] = datetime.datetime.now().isoformat()
+            if cmd:
+                log['cmd'] = cmd
+            if reply:
+                log['message'] = reply
+            print(jsn.dumps(log, indent=2))
+        return
+
 
 class Thread(threading.Thread):
     def __init__(self, i):
@@ -283,81 +283,12 @@ if __name__ == '__main__':
                         help='request timeout (default: 60)',  action="store")
     args = parser.parse_args()
 
-    threads = args.threads
-    socks = args.socks
-    ip = args.ip
-    port = args.port
     timeout_keyrock = args.timeout_keyrock
     timeout_orion = args.timeout_orion
     config_path = args.config_path
+
+    address = (args.ip, args.port)
     version_path = os.path.split(os.path.abspath(__file__))[0] + '/version'
-
-    address = (ip, port)
-
-    if not os.path.isfile(config_path):
-        print(jsn.dumps({'message': 'Config file not found', 'code': 500, 'cmd': 'start'}, indent=2))
-        config_file = None
-        sys.exit(1)
-    try:
-        with open(config_path) as f:
-            config = jsn.load(f)
-    except ValueError:
-        print(jsn.dumps({'message': 'Unsupported config type', 'code': 500, 'cmd': 'start'}, indent=2))
-        sys.exit(1)
-
-    version = dict()
-    if not os.path.isfile(version_path):
-        print(jsn.dumps({'message': 'Version file not found', 'code': 500, 'cmd': 'start'}, indent=2))
-        version_file = None
-        sys.exit(1)
-    try:
-        with open(version_path) as f:
-            version_file = f.read().split('\n')
-            version['build'] = version_file[0]
-            version['commit'] = version_file[1]
-    except IndexError:
-        print(jsn.dumps({'message': 'Unsupported version file type', 'code': 500, 'cmd': 'start'}, indent=2))
-        sys.exit(1)
-
-    data = dict()
-    try:
-        for endpoint in config['endpoints']:
-            i = len(data)
-            data[i] = dict()
-
-            data[i]["target"] = endpoint["target"]
-
-            if "timeout" not in endpoint:
-                data[i]["timeout"] = timeout_orion
-            else:
-                data[i]["timeout"] = endpoint["timeout"]
-
-            if 'entities' in endpoint:
-                data[i]['entities'] = dict()
-                for entity in endpoint['entities']:
-                    data[i]['entities'][entity['metric']] = dict()
-                    if 'id' in entity:
-                        data[i]['entities'][entity['metric']]["id"] = entity["id"]
-                    if 'type' in entity:
-                        data[i]['entities'][entity['metric']]["type"] = entity["type"]
-                    if 'service' in entity:
-                        data[i]['entities'][entity['metric']]["service"] = entity["service"]
-                    if 'path' in entity:
-                        data[i]['entities'][entity['metric']]["path"] = entity["path"]
-
-            if 'auth' in endpoint:
-                data[i]["keyrock"] = endpoint['auth']["keyrock"]
-                data[i]["client_id"] = endpoint['auth']["client_id"]
-                data[i]["client_secret"] = endpoint['auth']["client_secret"]
-                data[i]["username"] = endpoint['auth']["username"]
-                data[i]["password"] = endpoint['auth']["password"]
-    except KeyError:
-        print(jsn.dumps({'message': 'Config is not correct', 'code': 500, 'cmd': 'start'}, indent=2))
-        sys.exit(1)
-
-    if len(data) == 0:
-        print(jsn.dumps({'message': 'Endpoints list is empty', 'code': 500, 'cmd': 'start'}, indent=2))
-        sys.exit(1)
 
     schema = dict()
     schema['orion_failed_wrong_target'] = 0
@@ -380,14 +311,79 @@ if __name__ == '__main__':
 
     cmd_list = ['probe', 'ping', 'version']
 
+    if not os.path.isfile(config_path):
+        print(jsn.dumps({'message': 'Config file not found', 'code': 500, 'cmd': 'start'}, indent=2))
+        config_file = None
+        sys.exit(1)
+    try:
+        with open(config_path) as f:
+            cfg = jsn.load(f)
+    except ValueError:
+        print(jsn.dumps({'message': 'Unsupported config type', 'code': 500, 'cmd': 'start'}, indent=2))
+        sys.exit(1)
+
+    version = dict()
+    if not os.path.isfile(version_path):
+        print(jsn.dumps({'message': 'Version file not found', 'code': 500, 'cmd': 'start'}, indent=2))
+        version_file = None
+        sys.exit(1)
+    try:
+        with open(version_path) as f:
+            version_file = f.read().split('\n')
+            version['build'] = version_file[0]
+            version['commit'] = version_file[1]
+    except IndexError:
+        print(jsn.dumps({'message': 'Unsupported version file type', 'code': 500, 'cmd': 'start'}, indent=2))
+        sys.exit(1)
+
+    config = dict()
+    try:
+        for endpoint in cfg['endpoints']:
+            i = len(config)
+            config[i] = dict()
+
+            config[i]["target"] = endpoint["target"]
+
+            if "timeout" not in endpoint:
+                config[i]["timeout"] = timeout_orion
+            else:
+                config[i]["timeout"] = endpoint["timeout"]
+
+            if 'entities' in endpoint:
+                config[i]['entities'] = dict()
+                for entity in endpoint['entities']:
+                    config[i]['entities'][entity['metric']] = dict()
+                    if 'id' in entity:
+                        config[i]['entities'][entity['metric']]["id"] = entity["id"]
+                    if 'type' in entity:
+                        config[i]['entities'][entity['metric']]["type"] = entity["type"]
+                    if 'service' in entity:
+                        config[i]['entities'][entity['metric']]["service"] = entity["service"]
+                    if 'path' in entity:
+                        config[i]['entities'][entity['metric']]["path"] = entity["path"]
+
+            if 'auth' in endpoint:
+                config[i]["keyrock"] = endpoint['auth']["keyrock"]
+                config[i]["client_id"] = endpoint['auth']["client_id"]
+                config[i]["client_secret"] = endpoint['auth']["client_secret"]
+                config[i]["username"] = endpoint['auth']["username"]
+                config[i]["password"] = endpoint['auth']["password"]
+    except KeyError:
+        print(jsn.dumps({'message': 'Config is not correct', 'code': 500, 'cmd': 'start'}, indent=2))
+        sys.exit(1)
+
+    if len(config) == 0:
+        print(jsn.dumps({'message': 'Endpoints list is empty', 'code': 500, 'cmd': 'start'}, indent=2))
+        sys.exit(1)
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(address)
-    sock.listen(socks)
+    sock.listen(args.socks)
 
-    [Thread(i) for i in range(threads)]
+    [Thread(i) for i in range(args.threads)]
 
-    print(jsn.dumps({'message': 'Service started', 'code': 200, 'threads': threads, 'socks': socks}, indent=2))
+    print(jsn.dumps({'message': 'Service started', 'code': 200}, indent=2))
 
     while True:
         time.sleep(9999)
